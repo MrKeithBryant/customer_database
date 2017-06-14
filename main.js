247ba078d4db1d7a7f100f33576d0c2835c28d5f
Keith Bryant <kabryant@cinci.rr.com>
2017-06-14 16:22:55 -0400

    customer_database

---
+++ main.js
@@ -0,0 +1,16 @@
+// 1. Fetch your users data
+// 2. Loop over the data
+// 3. Display it in the `.customers` element
+//(function () {
+
+  //'use strict';
+
+  // Your Code Goes Here
+
+//})();
+
+(function () {
+
+    'use strict';
+
+  })();
